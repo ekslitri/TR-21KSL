4b6616cb470b4d5c2811e688d828a50665f04969
Katya <katyaslusar2004@gmail.com>
2024-11-25 04:24:49 +0200

    Finished implementation of FuncA with sum of first n elements and comments

--- MyClass.cpp
+++ MyClass.cpp
@@ -1,5 +1,19 @@
 #include "MyClass.h"
+#include <cmath> // Для використання pow
 
-double MyClass::FuncA() {
-    return 0;
+double factorial(int num) {
+    double fact = 1;
+    for (int i = 1; i <= num; ++i) {
+        fact *= i;
+    }
+    return fact;
 }
+
+double MyClass::FuncA(int n, double x) {
+    double result = 0;
+    for (int i = 0; i < n; ++i) {
+        result += pow(x, 2 * i) / factorial(2 * i);
+    }
+    return result;
+}
+
